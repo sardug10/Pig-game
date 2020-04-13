b97f0468fae8907c8091e3b6b84439aa4637ad6b
Sarthak Duggal <52778617+sardug10@users.noreply.github.com>
2020-04-13 19:54:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ GAME RULES:
 var scores,activePlayer,roundScore,gamePlaying=true;
 
 alert("GAME RULES: 1) The game has 2 players, playing in rounds");
-alert("2) In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score");
+alert("2) In each turn, a player rolls a dice as many times as he wishes. Each result get added to his ROUND score");
 alert("3) BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn");
 alert("4) The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn");
 alert("5) The first player to reach 100 points on GLOBAL score wins the game");
